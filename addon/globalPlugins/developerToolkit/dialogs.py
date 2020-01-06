47a315e60467fca45240ddf516f75bc3845bf73c
Andy Borka <ajborka@users.noreply.github.com>
2020-01-05 19:44:53 -0500

    Remove enable Developer toolkit features from settings dialog.
    Removed the enable/disable Developer toolkit features setting from the settings dialog found under NVDA menu>preferences>settings, then selecting Developer toolkit from the categories list. Users can still enable/disable Developer toolkit features by pressing alt+windows+k to enable it, then pressing alt+windows+k again to disable it.

--- addon/globalPlugins/developerToolkit/dialogs.py
+++ addon/globalPlugins/developerToolkit/dialogs.py
@@ -10,7 +10,6 @@ import config
 class DtkSettingsPanel(gui.SettingsPanel):
 	title = u"Developer toolkit"
 	def makeSettings(self, settingsSizer):
-		self.isEnabled = config.conf["developertoolkit"]["isEnabled"]
 		self.isDetailedMessages = config.conf["developertoolkit"]["isDetailedMessages"]
 		sHelper = gui.guiHelper.BoxSizerHelper(self, sizer = settingsSizer)
 		self.formattingAttributes = sHelper.addItem(wx.adv.EditableListBox(self, wx.ID_ANY, u"Included formatting attributes:", style = wx.adv.EL_DEFAULT_STYLE|wx.adv.EL_ALLOW_NEW|wx.adv.EL_ALLOW_EDIT|wx.adv.EL_ALLOW_DELETE))
@@ -22,14 +21,10 @@ class DtkSettingsPanel(gui.SettingsPanel):
 		self.formattingAttributes.SetStrings(config.conf["developertoolkit"]["fontInfo"])
 		self.colorDisplay = sHelper.addLabeledControl(u"Color display format:", wx.Choice, choices = ['RGB', 'Hex', 'Name'])
 		self.colorDisplay.SetSelection(config.conf["developertoolkit"]["displayColorFormat"])
-		# Enable or disable the addon.
-		self.isEnabledToggleButton = sHelper.addItem(wx.ToggleButton(self, label = u"Enable Developer toolkit features"))
-		self.isEnabledToggleButton.SetValue(self.isEnabled)
 		self.isDetailedMessagesToggleButton = sHelper.addItem(wx.ToggleButton(self, label = u"Messages are detailed."))
 		self.isDetailedMessagesToggleButton.SetValue(self.isDetailedMessages)
 
 	def onSave(self):
-		config.conf["developertoolkit"]["isEnabled"] = self.isEnabledToggleButton.GetValue()
 		config.conf["developertoolkit"]["isDetailedMessages"] = self.isDetailedMessagesToggleButton.GetValue()
 		config.conf["developertoolkit"]["fontInfo"] = self.formattingAttributes.GetStrings()
 		config.conf["developertoolkit"]["displayColorFormat"] = self.colorDisplay.GetSelection()
